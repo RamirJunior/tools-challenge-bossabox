b2421729b2c6e14dfca579cb2e81d9986b7bc9e7
Ramir Junior <junior.ribeiro0@live.com>
2023-11-19 04:13:12 -0300

    Initial commit

---
+++ src/main/java/br/com/ramir/toolschallengebossabox/ToolsChallengeBossaboxApplication.java
@@ -0,0 +1,13 @@
+package br.com.ramir.toolschallengebossabox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsChallengeBossaboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolsChallengeBossaboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ramir/toolschallengebossabox/ToolsChallengeBossaboxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ramir.toolschallengebossabox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsChallengeBossaboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
